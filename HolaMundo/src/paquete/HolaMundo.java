7c4cfd018c5d21bbee1eb90eeec833d751c85a35
GiL <GiL@GiL>
2018-02-21 23:14:43 +0100

    Version 2.0 María

--- HolaMundo/src/paquete/HolaMundo.java
+++ HolaMundo/src/paquete/HolaMundo.java
@@ -16,7 +16,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         System.out.println("¡Hola mundo!"); // Version 1.0
-        System.out.println("Texto de la versión 2.0 del proyecto"); // Version 2.0
+        System.out.println("Modificación de María"); // Version 2.0 Maria
     }
     
 }
